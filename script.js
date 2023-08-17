30c3d1302211c8350cf2aa61a681e5c0993db589
phogatheena <109434462+phogatheena@users.noreply.github.com>
2023-08-18 00:06:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
-	 return sampleObject.hasOwnProperty(key);
+	 return return key in sampleObject;//sampleObject.hasOwnProperty(key);
 }
 
 // Do not change the code below
